9bc100d61f5296e9bc2f25316a52c25412b2db22
thach90 <tkach90@yahoo.com.ua>
2020-03-22 23:28:50 +0200

    create Color Context, use Color Context to rebuild Button component and combine Language and Color Consumers to change color and language accordingly, wrap UserCreate component via ColorContext.Provider to have an access to Color Context in the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "./contexts/LanguageContext";
+import ColorContext from "./contexts/ColorContext";
 
 class App extends React.Component {
     state = {
-        language: 'english'
+        language: 'english',
+        color: 'primary'
     };
 
-    onLanguageChange = language => {
-        this.setState({ language });
+    onLanguageChange = (language, color) => {
+        this.setState({
+            language,
+            color
+        });
     };
 
     render() {
@@ -18,16 +23,18 @@ class App extends React.Component {
                     Select a language:
                     <i
                         className='flag us'
-                        onClick={() => this.onLanguageChange('english')}
+                        onClick={() => this.onLanguageChange('english', 'primary')}
                     />
                     <i
                         className='flag ua'
-                        onClick={() => this.onLanguageChange('ukrainian')}
+                        onClick={() => this.onLanguageChange('ukrainian', 'yellow')}
                     />
                 </div>
-                <LanguageContext.Provider value={this.state.language}>
-                    <UserCreate />
-                </LanguageContext.Provider>
+                <ColorContext.Provider value={this.state.color}>
+                    <LanguageContext.Provider value={this.state.language}>
+                        <UserCreate />
+                    </LanguageContext.Provider>
+                </ColorContext.Provider>
             </div>
         )
     }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import LanguageContext from "./contexts/LanguageContext";
+import ColorContext from "./contexts/ColorContext";
 
 class Button extends React.Component {
-    static contextType = LanguageContext;
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Підтвердити';
+    }
+
+    renderButton(color) {
+        return (
+            <button className={`ui button ${color}`}>
+                <LanguageContext.Consumer>
+                    {value => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
+            </button>
+        );
+    }
 
     render() {
-        const text = this.context === 'english' ? 'Submit' : 'Підтвердити';
-
         return (
             <>
-                <button className='ui button primary'>{text}</button>
+                <ColorContext.Consumer>
+                    {color => this.renderButton(color)}
+                </ColorContext.Consumer>
             </>
         )
     }
